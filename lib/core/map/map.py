be78a8730336269a13664b225096fcdc842d64ab
Baioretto <helloworldfirstfta@gmail.com>
2023-04-05 21:01:56 +0300

    Rewritten Map class

--- lib/core/map/map.py
+++ lib/core/map/map.py
@@ -1,8 +1,11 @@
 from .province import Province
 
 class Map:
-    def __init__(self, provinces: [Province]):
-        pass
+    def __init__(self):
+        self.provinces_graph = dict()
+    
+    def add_province(self, new_province: Province, neighboring_provinces: [Province]):
+        self.provinces_graph[new_province] = set(neighboring_provinces)
 
-    def __add_province(self, province):
-        pass
+        for pr in neighboring_provinces:
+            self.provinces_graph[pr].add(new_province)
