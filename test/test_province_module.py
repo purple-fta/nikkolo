75c485027d430b31f5444e21fd4f772581ab0cc4
Baioretto <helloworldfirstfta@gmail.com>
2023-04-17 13:38:05 +0300

    Delate test with error

--- test/test_province_module.py
+++ test/test_province_module.py
@@ -9,8 +9,6 @@ def test_create_province():
     with pytest.raises(ValueError):
         assert Province([123], ProvinceType.land.value, True)
 
-        assert Province("123", ProvinceType().land.value, True)
-        assert Province("123", ProvinceType().land, True)
         assert Province("123", ProvinceType.land, True)
         assert Province("123", 1, True)
         assert Province("123", "123", True)
