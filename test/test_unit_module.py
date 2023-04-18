9e50b3cb9c892bed4b835352ef18996ba2eef231
Baioretto <helloworldfirstfta@gmail.com>
2023-04-18 18:50:34 +0300

    Change tests name

--- test/test_unit_module.py
+++ test/test_unit_module.py
@@ -4,7 +4,7 @@ import pytest
 
 
 @pytest.mark.parametrize("location", [[1], 2, "3", True])
-def test_create_unit(location):
+def test_create_unit_with_type_error(location):
     assert Unit(Province("", ProvinceType.land.value, True))
 
     with pytest.raises(TypeError):
@@ -13,14 +13,14 @@ def test_create_unit(location):
 @pytest.mark.parametrize(("unit", "target"), [ [[1],   1],
                                                ["2",  [2]],
                                                [True, "3"] ])
-def test_create_move(unit, target):
+def test_create_move_with_type_error(unit, target):
     with pytest.raises(TypeError):
         assert Move(unit, target)
 
 @pytest.mark.parametrize(("unit", "target", "move_target"), [ [[1],   1,  '1'],
                                                               ["2",  [2],  2 ],
                                                               [True, "3", [3]] ])
-def test_create_support_move(unit, target, move_target):
+def test_create_support_move_with_type_error(unit, target, move_target):
     assert SupportMove(Unit(Province("", ProvinceType.land.value, True)), 
                        Province("", ProvinceType.land.value, True), 
                        Move(Unit(Province("", ProvinceType.land.value, True)), Province("", ProvinceType.land.value, True)))
@@ -31,7 +31,7 @@ def test_create_support_move(unit, target, move_target):
 @pytest.mark.parametrize(("unit", "target", "unit_target"), [ [[1],   1,  '1'],
                                                               ["2",  [2],  2 ],
                                                               [True, "3", [3]] ])
-def test_create_support_hold(unit, target, unit_target):
+def test_create_support_hold_with_type_error(unit, target, unit_target):
     assert SupportHold(Unit(Province("", ProvinceType.land.value, True)),
                        Province("", ProvinceType.land.value, True),
                        Unit(Province("", ProvinceType.land.value, True)))
