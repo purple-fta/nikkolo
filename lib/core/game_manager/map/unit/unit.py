5f7c874ba629e4d5cedf164bee603f62c2e1a185
Baioretto <helloworldfirstfta@gmail.com>
2023-04-19 12:26:47 +0300

    Passing tests
    1. Added type and value checking
    2. Change unit type system. Now you can force the type, but it can be self-defined

--- lib/core/game_manager/map/unit/unit.py
+++ lib/core/game_manager/map/unit/unit.py
@@ -14,7 +14,7 @@ class Unit:
         Args:
             location (Province): The province in which the unit is located
     """
-    def __init__(self, location: Province, unit_type: UnitType = UnitType.overland.value):
+    def __init__(self, location: Province, unit_type: UnitType = None):
         # Type checking
         if not issubclass(type(location), Province):
             raise TypeError("The first argument has the wrong type")
@@ -22,6 +22,9 @@ class Unit:
         self.location = location
         self.power = 1
         self.protection = 1
+
+        if not unit_type:
+            unit_type = UnitType.nautical.value if location.province_type == ProvinceType.water.value else UnitType.overland.value
 
         self.unit_type = unit_type
 
@@ -62,5 +65,27 @@ class SupportHold(Move):
 
 class ConvoyMove(Move):
     def __init__(self, unit: Unit, province_target: Province, ships: [Unit]):
+        # Type checking
+        if not issubclass(type(unit), Unit):
+            raise TypeError("The first argument has the wrong type")
+        if not issubclass(type(province_target), Province):
+            raise TypeError("The second argument has the wrong type")
+        if type(ships) == list:
+            for i in ships:
+                if not issubclass(type(i), Unit):
+                    raise TypeError("The third argument has the wrong type in list")
+        else:
+            raise TypeError("The third argument has the wrong type")
+
+        # Value checking
+        if unit.unit_type != UnitType.overland.value:
+            raise ValueError("Unit has bad type for convoy move")
+        if province_target.province_type != ProvinceType.coast.value:
+            raise ValueError("Target province has bad type for convoy move")
+        for ship in ships:
+            if ship.unit_type != UnitType.nautical.value:
+                raise ValueError("Ships has bad type in list")
+
+
         Move.__init__(self, unit, province_target)
         self.ships = ships
