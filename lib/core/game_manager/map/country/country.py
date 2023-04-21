502155f545203ee9bf9d60361a89b8bc9c92bba4
Baioretto <helloworldfirstfta@gmail.com>
2023-04-21 23:28:26 +0300

    Implementing the creation of an instance of the Country class
    1. Removed stability in the argument. Now it defaults to 0
    2. Added a list with the provinces that belong to the country
    3. Added value and type checking

--- lib/core/game_manager/map/country/country.py
+++ lib/core/game_manager/map/country/country.py
@@ -1,3 +1,22 @@
+from ..province import *
+
 class Country:
-    def __init__(self, name: str, stability: float):
+    def __init__(self, name: str, provinces: list):
+        
+        # Type checking
+        if type(name) != str:
+            raise TypeError("The first argument has the wrong type")
+        if type(provinces) != list:
+            raise TypeError("The second argument has the wrong type")
+
+        # Need to check the type in the list
+        for pr in provinces:
+            if not issubclass(type(pr), Province):
+                raise TypeError("The second argument has the wrong type")
+            elif pr.province_type == ProvinceType.water.value:
+                raise ValueError("A water province cannot belong to a country's territory")
+                
+
         self.name = name
+        self.provinces = provinces
+        self.stability = 0
